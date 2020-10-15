14ca87c61df10d3909f04a96b2b80203d192bdb5
Stephen Szabo <DOSteveSz@gmail.com>
2020-10-15 16:24:57 -0400

    Cleanup of unnecessary files

--- Startup.cs
+++ Startup.cs
@@ -29,7 +29,6 @@ namespace TravelersChosen
             services.AddRazorPages();
             services.AddHttpClient();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
             services.AddSingleton<ClanMemberService>();
         }
 
