6805b849f322336380b338c2842d8f36dcf86202
Stephen Szabo <DOSteveSz@gmail.com>
2020-10-15 13:39:36 -0400

    Created blazor project and began coding

---
+++ Data/ClanMemberService.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace TravelersChosen.Data
+{
+    public class ClanMemberService
+    {
+        private readonly IHttpClientFactory _clientFactory;
+        public List<ClanMemeber> ClanMembers { get; private set; }
+
+        public ClanMemberService(IHttpClientFactory clientFactory)
+        {
+            _clientFactory = clientFactory;
+        }
+        public async Task GetClanMembers()
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, "https://www.bungie.net/Platform/GroupV2/4328049/Members/");
+            request.Headers.Add("X-API-Key", "8407f68ab5d84ed0a9026514efb6bbf2");
+            var client = _clientFactory.CreateClient("bungie");
+            var response = await client.SendAsync(request);
+            if(response.IsSuccessStatusCode)
+            {
+                var responseStream = await response.Content.ReadAsStringAsync();
+                var responseList = JsonConvert.DeserializeObject<ResponseData>(responseStream);
+                ClanMembers = responseList.Response.results;
+
+            }
+            else
+            {
+                ClanMembers = new List<ClanMemeber>();
+            }
+        }
+    }
+}
---
+++ Data/ClanMemeber.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TravelersChosen.Data
+{
+    public class DestinyUserInfo
+    {
+        public string LastSeenDisplayName { get; set; }
+        public int LastSeenDisplayNameType { get; set; }
+        public string iconPath { get; set; }
+        public int crossSaveOverride { get; set; }
+        public List<int> applicableMembershipTypes { get; set; }
+        public bool isPublic { get; set; }
+        public int membershipType { get; set; }
+        public string membershipId { get; set; }
+        public string displayName { get; set; }
+    }
+
+    public class BungieNetUserInfo
+    {
+        public string supplementalDisplayName { get; set; }
+        public string iconPath { get; set; }
+        public int crossSaveOverride { get; set; }
+        public bool isPublic { get; set; }
+        public int membershipType { get; set; }
+        public string membershipId { get; set; }
+        public string displayName { get; set; }
+    }
+
+    public class ClanMemeber
+    {
+        public int memberType { get; set; }
+        public bool isOnline { get; set; }
+        public string lastOnlineStatusChange { get; set; }
+        public string groupId { get; set; }
+        public DestinyUserInfo destinyUserInfo { get; set; }
+        public BungieNetUserInfo bungieNetUserInfo { get; set; }
+        public DateTime joinDate { get; set; }
+    }
+
+    public class Query
+    {
+        public int itemsPerPage { get; set; }
+        public int currentPage { get; set; }
+    }
+
+    public class Response
+    {
+        public List<ClanMemeber> results { get; set; }
+        public int totalResults { get; set; }
+        public bool hasMore { get; set; }
+        public Query query { get; set; }
+        public bool useTotalResults { get; set; }
+    }
+
+    public class MessageData
+    {
+    }
+
+    public class ResponseData
+    {
+        public Response Response { get; set; }
+        public int ErrorCode { get; set; }
+        public int ThrottleSeconds { get; set; }
+        public string ErrorStatus { get; set; }
+        public string Message { get; set; }
+        public MessageData MessageData { get; set; }
+    }
+}
